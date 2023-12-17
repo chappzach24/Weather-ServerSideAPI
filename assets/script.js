f939b93e374b523f31fb7e801e1284c574ce277b
zachary chapman <zach.chapman24@gmail.com>
2023-12-17 15:45:11 -0500

    started api fetch

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 var APIKey = "bb3db924a5328b1a5ee83d1241a27223";
 var city;
 
+let cityName = "Gahanna";
+
+//df43dac8214f8ed2c98fb6948c2f6dff
+
 // Function to save user input
 function saveInput() {
   // Get user input
@@ -9,7 +13,9 @@ function saveInput() {
   // Check if the input is not empty
   if (userInput.trim() !== "") {
     // Save the input to localStorage (you can use other storage options as well)
-    var history = localStorage.getItem("inputHistory") ? JSON.parse(localStorage.getItem("inputHistory")) : [];
+    var history = localStorage.getItem("inputHistory")
+      ? JSON.parse(localStorage.getItem("inputHistory"))
+      : [];
     history.push(userInput);
     localStorage.setItem("inputHistory", JSON.stringify(history));
 
@@ -27,7 +33,9 @@ function updateHistory() {
   historyList.innerHTML = "";
 
   // Retrieve the input history from localStorage
-  var history = localStorage.getItem("inputHistory") ? JSON.parse(localStorage.getItem("inputHistory")) : [];
+  var history = localStorage.getItem("inputHistory")
+    ? JSON.parse(localStorage.getItem("inputHistory"))
+    : [];
 
   // Display the history in the list
   history.forEach(function (item) {
@@ -41,3 +49,25 @@ function updateHistory() {
 window.onload = function () {
   updateHistory();
 };
+
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},&appid=bb3db924a5328b1a5ee83d1241a27223`;
+
+// Use the fetch function to make a GET request to the API
+fetch(apiUrl)
+  .then((response) => {
+    // Check if the request was successful (status code 200)
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    // Parse the JSON response
+    return response.json();
+  })
+  .then((data) => {
+    // Do something with the data
+    console.log(data.list[0].weather);
+  })
+  .catch((error) => {
+    // Handle any errors that occurred during the fetch
+    console.error("Fetch error:", error);
+  });
