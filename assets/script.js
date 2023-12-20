d81844b54d5362bf9401e7c2cadf0f796549561b
zachary chapman <zach.chapman24@gmail.com>
2023-12-19 19:10:00 -0500

    fix flow of js

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,86 @@
-var api = 'https://api.openweathermap.org/data/2.5/weather?q='
-let city = "Gahanna";
+//&deg; symbol
+
+var api = "https://api.openweathermap.org/data/2.5/";
 var APIKey = "&appid=bb3db924a5328b1a5ee83d1241a27223";
 var units = "&units=imperial";
+var historyArr =  JSON.parse(localStorage.getItem("inputHistory")) || [];
 
-var url = api + city + APIKey + units;
+
 
 // Function to save user input
 function saveInput() {
   // Get user input
   var userInput = document.getElementById("userInput").value;
 
-  // Check if the input is not empty
-  if (userInput.trim() !== "") {
-    // Save the input to localStorage (you can use other storage options as well)
-    var history = localStorage.getItem("inputHistory")
-      ? JSON.parse(localStorage.getItem("inputHistory"))
-      : [];
-    history.push(userInput);
-    localStorage.setItem("inputHistory", JSON.stringify(history));
+  if(userInput === ''){
+    return
+  }
 
-    // Clear the input field
-    document.getElementById("userInput").value = "";
+  runData(userInput);
+
+  // Clear the input field
+  document.getElementById("userInput").value = "";
+}
+
+function runData(y) {
+  runCurrent(y);
+  runForecast(y);
+}
+
+function runCurrent(x) {
+  var url = api + "weather?q=" + x + APIKey + units;
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      
+      if(data.message === 'city not found'){
+        return
+      }
+      let currentDay = data.main
+      console.log(`current temp: ${currentDay.temp}, current wind: ${data.wind.speed}`);
+
+      saveToStorage(data.name)
+      // saveToStorage(data.)
+    })
+}
+
+function runForecast(a) {
+  var url = api + "forecast?q=" + a + APIKey + units;
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if(data.message === 'city not found'){
+        return
+      }
+      console.log(data);
+      let dayArray = []
+      let days = ["Day 1", "Day 2", "Day 3","Day 4","Day 5"]
+      for (hour of data.list){
+        if (hour.dt_txt.split(" ")[1] === "12:00:00"){
+          dayArray.push(hour)
+        }
+      }
+      console.log(dayArray)
+      
+    });
+}
+
+function saveToStorage(city) {
+  // // Check if the input is not empty
+
+    // Save the input to localStorage (you can use other storage options as well)
+    historyArr.push(city);
+    localStorage.setItem("inputHistory", JSON.stringify(historyArr));
 
     // Update the history list
     updateHistory();
-  }
+  
 }
 
 // Function to update the history list
@@ -32,27 +88,14 @@ function updateHistory() {
   var historyList = document.getElementById("historyList");
   historyList.innerHTML = "";
 
-  // Retrieve the input history from localStorage
-  var history = localStorage.getItem("inputHistory")
-    ? JSON.parse(localStorage.getItem("inputHistory"))
-    : [];
+  
 
   // Display the history in the list
-  history.forEach(function (item) {
+  historyArr.forEach(function (item) {
     var li = document.createElement("li");
     li.textContent = item;
     historyList.appendChild(li);
   });
 }
 
-// Load the initial history on page load
-window.onload = function () {
-  updateHistory();
-};
-
-fetch(url)
-  .then(function (response) {
-    return response.json();
-  })
-  
-
+updateHistory()
