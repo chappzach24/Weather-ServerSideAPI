29606036c9822a33260b41b27d23b0df1b4d8690
zachary chapman <zach.chapman24@gmail.com>
2024-01-04 20:09:45 -0500

    fixed btn to show state that was clicked

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
-//&deg; symbol
-
 var api = "https://api.openweathermap.org/data/2.5/";
 var APIKey = "&appid=bb3db924a5328b1a5ee83d1241a27223";
 var units = "&units=imperial";
@@ -8,16 +6,9 @@ var weatherIcon = document.getElementById("weatherIcon");
 var userInput;
 
 const timeEl = document.querySelector(".time");
-const questPanel = document.getElementById("quesPanel");
-
-const now = new Date();
-const currentDateTime = now.toLocaleString();
-
-console.log(currentDateTime); // output: "7/20/2021, 2:28:15 PM"
 
 // Function to save user input
 function saveInput() {
-  // Get user input
   userInput = document.getElementById("userInput").value;
 
   document.getElementById("noShowCurrent").classList.remove("hidden");
@@ -29,20 +20,19 @@ function saveInput() {
 
   runData(userInput);
   saveToStorage(userInput);
-  // Clear the input field
   document.getElementById("userInput").value = "";
 }
 
-function runData(y) {
-  runCurrent(y);
-  runForecast(y);
+function runData(city) {
+  runCurrent(city);
+  runForecast(city);
 }
 
 function Show() {}
 
 //fetch for current weather
-function runCurrent(x) {
-  var url = api + "weather?q=" + x + APIKey + units;
+function runCurrent(city) {
+  var url = api + "weather?q=" + city + APIKey + units;
 
   fetch(url)
     .then(function (response) {
@@ -57,13 +47,13 @@ function runCurrent(x) {
         `current temp: ${currentDay.temp}, current wind: ${data.wind.speed}`
       );
       console.log(data);
-      weatherDOM(data, userInput, currentDateTime);
+      weatherDOM(data, city);
     });
 }
 
-//fetch for 5 day forcast
-function runForecast(a) {
-  var url = api + "forecast?q=" + a + APIKey + units;
+//fetch for 5 day forecast
+function runForecast(city) {
+  var url = api + "forecast?q=" + city + APIKey + units;
 
   fetch(url)
     .then(function (response) {
@@ -73,10 +63,9 @@ function runForecast(a) {
       if (data.message === "city not found") {
         return;
       }
-      //makes array for forcast to pull data at 12:00
+      //makes array for forecast to pull data at 12:00
       console.log(data);
       let dayArray = [];
-      let days = ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5"];
       for (hour of data.list) {
         if (hour.dt_txt.split(" ")[1] === "12:00:00") {
           dayArray.push(hour);
@@ -84,18 +73,12 @@ function runForecast(a) {
       }
       console.log(dayArray);
       forcastDOM(dayArray);
-      // forcastDOM(data);
     });
 }
 
 function saveToStorage(city) {
-  // // Check if the input is not empty
-
-  // Save the input to localStorage (you can use other storage options as well)
   historyArr.push(city);
   localStorage.setItem("inputHistory", JSON.stringify(historyArr));
-
-  // Update the history list
   updateHistory();
 }
 
@@ -103,7 +86,6 @@ function updateHistory() {
   var historyList = document.getElementById("historyList");
   historyList.innerHTML = "";
 
-  // Display the history as buttons in the list
   historyArr.forEach(function (item) {
     var btn = document.createElement("button");
     btn.textContent = item;
@@ -111,31 +93,16 @@ function updateHistory() {
       runData(item);
     });
 
-    //makes user input into a button
     var li = document.createElement("li");
     li.appendChild(btn);
     historyList.appendChild(li);
   });
 }
 
-
-// Function to update the history list
-// function updateHistory(userInput) {
-//   var historyList = document.getElementById("historyList");
-//   historyList.innerHTML = "";
-
-//   // Display the history in the list
-//   historyArr.forEach(function (item) {
-//     var li = document.createElement("li");
-//     li.textContent = item;
-//     historyList.appendChild(li);
-//   });
-// }
-
 //weather for current day
-function weatherDOM(data, userInput, currentDateTime) {
+function weatherDOM(data, city) {
   var currentTime = document.getElementById("currentTime");
-  currentTime.textContent = currentDateTime;
+  currentTime.textContent = new Date().toLocaleString();
 
   var iconcode = data.weather[0].icon;
   var iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
@@ -144,7 +111,7 @@ function weatherDOM(data, userInput, currentDateTime) {
   weatherIcon.style.width;
 
   var currentstate = document.getElementById("state");
-  let result = userInput.toUpperCase();
+  let result = city.toUpperCase();
   currentstate.textContent = result;
 
   var currentTemp = document.getElementById("temp1");
@@ -157,125 +124,32 @@ function weatherDOM(data, userInput, currentDateTime) {
   currentHum.textContent = data.main.humidity;
 }
 
-
-//weather forcast for 5 days
+//weather forecast for 5 days
 function forcastDOM(dayArray) {
-  // day 2 of forecast
+  for (let dayIndex = 0; dayIndex < dayArray.length; dayIndex++) {
+    const timeDate = dayArray[dayIndex].dt_txt;
+    let newTimeDate = timeDate.split(' ');
 
-  const timeDate = dayArray[0].dt_txt;
-  let newTimeDate = timeDate.split(' ')
-  console.log(newTimeDate);
+    var dayTimeElement = document.getElementById("day" + (dayIndex + 2));
+    dayTimeElement.textContent = newTimeDate[0];
 
-  var day2Time = document.getElementById("day2");
-  day2Time.textContent = newTimeDate[0];
+    var iconcode = dayArray[dayIndex].weather[0].icon;
+    var iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
-  var iconcode = dayArray[0].weather[0].icon;
-  var iconUrl2 = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    var weatherIconElement = document.getElementById("weatherIcon" + (dayIndex + 2));
+    weatherIconElement.setAttribute("src", iconUrl);
+    weatherIconElement.style.width;
 
-  weatherIcon2.setAttribute("src", iconUrl2);
-  weatherIcon2.style.width;
+    var tempElement = document.getElementById("temp" + (dayIndex + 2));
+    tempElement.textContent = dayArray[dayIndex].main.temp;
 
-  var day2Temp = document.getElementById("temp2");
-  day2Temp.textContent = dayArray[0].main.temp;
+    var windElement = document.getElementById("wind" + (dayIndex + 2));
+    windElement.textContent = dayArray[dayIndex].wind.speed;
 
-  var day2Wind = document.getElementById("wind2");
-  day2Wind.textContent = dayArray[0].wind.speed;
-
-  var day2Hum = document.getElementById("humidity2");
-  day2Hum.textContent = dayArray[0].main.humidity;
-
-  // day 3
-
-  const timeDate3 = dayArray[1].dt_txt;
-  let newTimeDate3 = timeDate3.split(' ')
-  console.log(newTimeDate3);
-
-  var day2Time = document.getElementById("day3");
-  day2Time.textContent = newTimeDate3[0];
-
-  var iconcode = dayArray[1].weather[0].icon;
-  var iconUrl3 = "http://openweathermap.org/img/w/" + iconcode + ".png";
-
-  weatherIcon3.setAttribute("src", iconUrl3);
-  weatherIcon3.style.width;
-
-  var day3Temp = document.getElementById("temp3");
-  day3Temp.textContent = dayArray[1].main.temp;
-
-  var day3Wind = document.getElementById("wind3");
-  day3Wind.textContent = dayArray[1].wind.speed;
-
-  var day3Hum = document.getElementById("humidity3");
-  day3Hum.textContent = dayArray[1].main.humidity;
-
-  //day 4
-  const timeDate4 = dayArray[2].dt_txt;
-  let newTimeDate4 = timeDate4.split(' ');
-
-  var day2Time = document.getElementById("day4");
-  day2Time.textContent = newTimeDate4[0];
-
-  var iconcode = dayArray[2].weather[0].icon;
-  var iconUrl4 = "http://openweathermap.org/img/w/" + iconcode + ".png";
-
-  weatherIcon4.setAttribute("src", iconUrl4);
-  weatherIcon4.style.width;
-
-  var day4Temp = document.getElementById("temp4");
-  day4Temp.textContent = dayArray[2].main.temp;
-
-  var day4Wind = document.getElementById("wind4");
-  day4Wind.textContent = dayArray[2].wind.speed;
-
-  var day4Hum = document.getElementById("humidity4");
-  day4Hum.textContent = dayArray[2].main.humidity;
-
-  //day 5
-
-  const timeDate5 = dayArray[3].dt_txt;
-  let newTimeDate5 = timeDate5.split(' ');
-
-  var day2Time = document.getElementById("day5");
-  day2Time.textContent = newTimeDate5[0];
-
-  var iconcode = dayArray[3].weather[0].icon;
-  var iconUrl5 = "http://openweathermap.org/img/w/" + iconcode + ".png";
-
-  weatherIcon5.setAttribute("src", iconUrl5);
-  weatherIcon5.style.width;
-
-
-  var day5Temp = document.getElementById("temp5");
-  day5Temp.textContent = dayArray[3].main.temp;
-
-  var day5Wind = document.getElementById("wind5");
-  day5Wind.textContent = dayArray[3].wind.speed;
-
-  var day5Hum = document.getElementById("humidity5");
-  day5Hum.textContent = dayArray[3].main.humidity;
-
-  //day 6
-
-  const timeDate6 = dayArray[4].dt_txt;
-  let newTimeDate6 = timeDate6.split(' ');
-
-  var day2Time = document.getElementById("day6");
-  day2Time.textContent = newTimeDate6[0];
-
-  var iconcode = dayArray[4].weather[0].icon;
-  var iconUrl6 = "http://openweathermap.org/img/w/" + iconcode + ".png";
-
-  weatherIcon6.setAttribute("src", iconUrl6);
-  weatherIcon6.style.width;
-
-  var day6Temp = document.getElementById("temp6");
-  day6Temp.textContent = dayArray[4].main.temp;
-
-  var day6Wind = document.getElementById("wind6");
-  day6Wind.textContent = dayArray[4].wind.speed;
-
-  var day6Hum = document.getElementById("humidity6");
-  day6Hum.textContent = dayArray[4].main.humidity;
+    var humidityElement = document.getElementById("humidity" + (dayIndex + 2));
+    humidityElement.textContent = dayArray[dayIndex].main.humidity;
+  }
 }
 
+// Initial call to update the history
 updateHistory();
